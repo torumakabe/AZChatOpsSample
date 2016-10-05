3edbc75e3491cf80cb685b4032ce06ab7930df8e
Toru Makabe <tomakabe@microsoft.com>
2016-10-05 14:50:09 +0900

    fix bug

--- postJob/index.js
+++ postJob/index.js
@@ -15,7 +15,7 @@ nconf.argv()
 const queue = Queue(nconf.get('STORAGE_ACCOUNT'), nconf.get('STORAGE_ACCOUNT_KEY'), 'azure-runslash-jobs');
 queue.create()
   .catch((err) => { 
-    throw err;
+    context.log(err);
   });
 
 //Main flow
@@ -129,9 +129,7 @@ const Queue = (accountName, accountKey, queueName) => {
     
     send: (msg) => {   
       return new Promise((resolve, reject) => {
-        try { 
-          logger.debug('Sending message:', msg);
-          
+        try {           
           queueService.createMessage(queueName, JSON.stringify(msg, null, 2), { messagettl: 240 * 60 }, (err) => {
             if (err) {
               return reject(err);
@@ -146,10 +144,7 @@ const Queue = (accountName, accountKey, queueName) => {
     
     update: (msg, contents) => {
       return new Promise((resolve, reject) => {
-        try { 
-          logger.debug('Updating message:', msg);
-          logger.debug(' > Contents:', contents);
-          
+        try {           
           queueService.updateMessage(queueName, msg.messageid, msg.popreceipt, 5, { messageText: JSON.stringify(contents, null, 2) }, (err, res) => {
             if (err) {
               return reject(err);
@@ -166,8 +161,6 @@ const Queue = (accountName, accountKey, queueName) => {
     delete: (msg) => {   
       return new Promise((resolve, reject) => {
         try { 
-          logger.debug('Deleting message:', msg);
-          
           queueService.deleteMessage(queueName, msg.messageid, msg.popreceipt, (err, res) => {
             if (err) {
               return reject(err);
@@ -183,8 +176,6 @@ const Queue = (accountName, accountKey, queueName) => {
     getMessages: (messageCallback) => {  
       return new Promise((resolve, reject) => {
         try { 
-          logger.debug('Getting messages...');
-          
           queueService.getMessages(queueName, { numOfMessages: 15, visibilityTimeout: 5 }, (err, result, response) => {
             if (err) {
               return reject(err);
@@ -193,8 +184,6 @@ const Queue = (accountName, accountKey, queueName) => {
             if (!result || !result.length) {
               return resolve();
             }
-            
-            logger.debug('Received:', result);
             
             async.each(result, messageCallback, (err) => {
               if (err) {
@@ -214,8 +203,6 @@ const Queue = (accountName, accountKey, queueName) => {
       return new Promise((resolve, reject) => {
         const processMessages = () => {
           client.getMessages(messageCallback)
-//            .delay(20 * 1000)
-//            .then(processMessages)
             .catch(reject);
         };
         processMessages();
